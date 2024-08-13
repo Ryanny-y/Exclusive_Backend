f1e28635ae0d2dd835d4af8c2bb1f652a4aa3b32
ryanny <ryannymromero@gmail.com>
2024-08-13 05:48:15 -0800

    add list

--- config/allowedList.js
+++ config/allowedList.js
@@ -1,3 +1,10 @@
-const allowedList = ['http://localhost:5173/Exclusive-React/', 'http://localhost:3500', 'https://vs3162mm-5173.asse.devtunnels.ms', 'https://vs3162mm-5173.asse.devtunnels.ms/Exclusive-React/', 'http://localhost:5173', 'https://ryanny-y.github.io/Exclusive-React/'];
+const allowedList = [
+    'http://localhost:5173/Exclusive-React/', 
+    'http://localhost:3500', 
+    'https://vs3162mm-5173.asse.devtunnels.ms', 
+    'https://vs3162mm-5173.asse.devtunnels.ms/Exclusive-React/', 
+    'http://localhost:5173', 
+    'https://ryanny-y.github.io/Exclusive-React/'
+  ];
 
 module.exports = { allowedList };
