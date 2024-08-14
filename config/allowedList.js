1de851267d9404c23490b51c1b3ac6796c418239
ryanny <ryannymromero@gmail.com>
2024-08-14 05:37:59 -0800

    Change allowed Origin

--- config/allowedList.js
+++ config/allowedList.js
@@ -1,10 +1,8 @@
 const allowedList = [
     'http://localhost:5173/Exclusive-React/', 
-    'http://localhost:3500', 
-    'https://vs3162mm-5173.asse.devtunnels.ms', 
-    'https://vs3162mm-5173.asse.devtunnels.ms/Exclusive-React/', 
-    'http://localhost:5173', 
-    'https://ryanny-y.github.io/Exclusive-React/'
+    'http://localhost:5173',
+    'https://ryanny-y.github.io/Exclusive-React/',
+    'https://ryanny-y.github.io'
   ];
 
 module.exports = { allowedList };
