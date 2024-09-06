8a782526271378b351ebb22448b963c1ce085942
Yanira <152132612+Ryanny-y@users.noreply.github.com>
2024-09-06 06:08:45 +0400

    Update orders.js

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -4,8 +4,8 @@ const verifyJWT = require('../../middlewares/verifyJWT');
 const { getOrders, createOrder, cancelOrder } = require('../../controller/api/orderController');
 
 router.route('/')
-  .get(verifyJWT, getOrders)
   .post(verifyJWT, createOrder)
   .delete(verifyJWT, cancelOrder);
 
+router.get('/:userId', verifyJWT, getOrders);
 module.exports = router;
